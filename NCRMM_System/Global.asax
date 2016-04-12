cca204a8a5d6c6b2fa3520740e69587c82a10f58
musfiqurcse <md.musfiqurcse@outlook.com>
2016-04-12 08:43:42 +0600

    First commit updated from the scratch

---
+++ NCRMM_System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCRMM_System.MvcApplication" Language="C#" %>
---
+++ NCRMM_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCRMM_System.Startup))]
+namespace NCRMM_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
